aed8e75f35373cc2ac6d286277443531691b5c23
dan <452260570@qq.com>
2018-03-10 14:32:51 +0800

    add test case and circleci

--- README.md
+++ README.md
@@ -1,18 +1,42 @@
 # Algorithm
 Practise one algorithm one day.
 
-### 基本概念
+## 基本概念
 >一个排序算法是一种将一串数据依照特定排序方式进行排列的一种算法，最常用到的排序方式是数值顺序以及字典顺序。    
 排序算法的输出必须遵守下列两个原则：
 * 输出结果为递增序列（递增是针对所需的排序顺序而言）
 * 输出结果是原输入的一种排列或是重组
 
 
-* [Quick Sort](https://github.com/ThreeBearsDan/Algorithm/tree/master/quicksort)
-* [Bubble Sort](https://github.com/ThreeBearsDan/Algorithm/tree/master/bubblesort)
-* [Insertion Sort](https://github.com/ThreeBearsDan/Algorithm/tree/master/insertionsort)
-* [Shell Sort](https://github.com/ThreeBearsDan/Algorithm/tree/master/shellsort)
-* [Heap Sort](https://github.com/ThreeBearsDan/Algorithm/tree/master/heapsort)
-* [Merge Sort](https://github.com/ThreeBearsDan/Algorithm/tree/master/mergesort)
+### [Quick Sort]
+#### 快速排序的基本思想：
+1. 通过一趟排序将要排序的数据分割成独立的两部分，其中一部分数据比另一部分数据都要小。
+2. 按此方法对这两部分数据分别进行快速排序，整个排序过程可以递归进行，以此达到整个数据变成有序序列。
 
+### [Bubble Sort]
+#### 冒泡排序基本思想：
+1. 重复地走访排序的数列，一次比较两个元素，如果它们的顺序错误就把它们交换过来。
+2. 走访数列的工作重复地进行直到再没有需要交换的元素。
 
+### [Insertion Sort]
+#### 插入排序基本思想：
+1. 通过构建有序序列，将未排序数据，在已排序序列中从后向前扫描，找到相应的位置并插入。
+
+### [Shell Sort]
+#### 希尔排序基本思想：
+希尔排序是对直接插入排序的一种改进，增大了元素移动的步长。
+1. 选定一个步长，一般为序列长度的一半。
+2. 按指定的步长将序列分组，使得每组以直接插入排序的方式排序。
+3. 逐渐缩短步长，重复1，2两步，使得最终步长为1。
+
+### [Heap Sort]
+#### 堆排序基本思想：
+1. 将堆的末端子节点进行调整，使得子节点永远小于父节点。
+2. 将堆的所有节点重新排序。
+3. 移除跟节点，并作最大堆调整递归运算。
+
+### [Merge Sort]
+#### 归并排序基本思想：
+1. 将序列相邻的两个元素进行归并操作，形成ceil(n/2)个序列，每个序列包含2或1个元素。
+2. 如果此时序列数不为1则将上述序列再次归并，形成ceil(n/4)个序列，每个序列包含4或3个元素。
+3. 重复步骤2，直到所有的元素排序完毕，即序列数为1。
---
+++ sort/sort.go
@@ -0,0 +1,133 @@
+package sort
+
+// 冒泡排序
+func BubbleSort(list []int) {
+	for i := 0; i < len(list); i++ {
+		for j := 1; j < len(list)-i; j++ {
+			if list[j-1] > list[j] {
+				list[j-1], list[j] = list[j], list[j-1]
+			}
+		}
+	}
+}
+
+// 堆排序
+func HeapSort(list []int) {
+	// 构建最大堆
+	l := len(list)
+	m := l / 2
+	for i := m; i > -1; i-- {
+		heap(list, i, l-1)
+	}
+
+	// 交换根尾节点并删除根节点
+	for i := l - 1; i > 0; i-- {
+		list[i], list[0] = list[0], list[i]
+		heap(list, 0, i-1)
+	}
+
+}
+
+func heap(list []int, i, end int) {
+	// 左节点
+	ln := 2*i + 1
+	if ln > end {
+		return
+	}
+	n := ln
+
+	// 右节点
+	rn := 2*i + 2
+	if rn <= end && list[rn] > list[n] {
+		n = rn
+	}
+
+	// 调整根节点
+	if list[i] < list[n] {
+		list[i], list[n] = list[n], list[i]
+	}
+	// 对所有节点进行调整
+	heap(list, n, end)
+}
+
+// 插入排序
+func InsertionSort(list []int) {
+	for i := 1; i < len(list); i++ {
+		for j := i; j > 0; j-- {
+			if list[j] < list[j-1] {
+				list[j], list[j-1] = list[j-1], list[j]
+			}
+		}
+	}
+}
+
+// 归并排序
+func MergeSort(list []int) []int {
+	n := len(list)
+	if n < 2 {
+		return list
+	}
+
+	mid := n / 2
+
+	llist := MergeSort(list[:mid])
+	rlist := MergeSort(list[mid:])
+
+	return merge(llist, rlist)
+}
+
+func merge(a []int, b []int) []int {
+	temp := make([]int, 0)
+	i, j := 0, 0
+	for i < len(a) && j < len(b) {
+		if a[i] < b[j] {
+			temp = append(temp, a[i])
+			i++
+		} else {
+			temp = append(temp, b[j])
+			j++
+		}
+	}
+	temp = append(temp, a[i:]...)
+	temp = append(temp, b[j:]...)
+
+	return temp
+}
+
+// 快速排序
+func QuickSort(list []int) []int {
+	if len(list) > 1 {
+		rlist := make([]int, 0)
+		llist := make([]int, 0)
+		key := make([]int, 0)
+		key = append(key, list[0])
+		for i := 1; i < len(list); i++ {
+			if list[i] < key[0] {
+				llist = append(llist, list[i])
+			} else if list[i] > key[0] {
+				rlist = append(rlist, list[i])
+			} else {
+				key = append(key, list[i])
+			}
+		}
+		return append(QuickSort(llist), append(key, QuickSort(rlist)...)...)
+	}
+	return list
+}
+
+// 希尔排序
+func ShellSort(list []int) {
+	n := len(list)
+	gap := n / 2
+
+	for gap > 0 {
+		for i := gap; i < n; i++ {
+			for j := i; j > gap-1; j -= gap {
+				if list[j] < list[j-gap] {
+					list[j], list[j-gap] = list[j-gap], list[j]
+				}
+			}
+		}
+		gap = gap / 2
+	}
+}
